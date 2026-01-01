ed3c05fe016d514481ee5d4fa313b0d3570a8c7d
Angelie <angelieyu@gmail.com>
2026-01-01 11:54:21 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import reactLogo from './assets/angelieyuLogo.png'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        
+        <div className='logo'>
+          <img src={reactLogo} className="logo react" alt="React logo" /> <h2>Welcome to ANGELIEYU.com</h2>
+        </div>
+      </div>
+     
+      
+    </>
+  )
+}
+
+export default App
